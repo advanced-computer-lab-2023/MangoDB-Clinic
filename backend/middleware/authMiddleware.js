b33c8555fc32497ac6d766328b2c9a3942160198
Kareem Fayed <kareemashraffayed@gmail.com>
2023-10-08 01:49:56 +0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,8 +1,28 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
+const Patient = require('../models/patientModel')
+const Doctor = require('../models/doctorModel')
 
-const protect = asyncHandler( async (req, res, next) => {
+const authenticate = asyncHandler( async (req, res, next) => {
+
+    const header = req.headers.authorization
+
+    if(header && header.startsWith('Bearer')){
+        try {
+            token = header.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.SECRET)
+
+            req.user = await Patient.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            res.status(401).json({ error: 'Not Authorized!' })
+        }
+    }
+
+    if(!token)
+        res.status(401).json({ error: 'Authorization Error. No Token!' })
     
 })
 
-module.exports = {protect}
+module.exports = authenticate
