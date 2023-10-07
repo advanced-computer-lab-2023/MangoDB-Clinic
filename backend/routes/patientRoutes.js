ef0f3017c07121c69b0359ee4767bb51b30fb680
Kareem Gabr <Kareemjabr26@gmail.com>
2023-10-07 21:45:15 +0300

    CRUD APIs added to routes

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -3,4 +3,27 @@ const router = express.Router()
 
 const {protect} = require('../middleware/authMiddleware')
 
+const {
+    getAllPatients,
+    getPatient,
+    addPatient,
+    updatePatient,
+    deletPatient
+} = require('../controllers/patientController')
+
+//GET all patients
+router.get('/getAllPatients', getAllPatients)
+
+//GET a single patient
+router.get('/getPatient/:id', getPatient)
+
+//POST a single patient
+router.get('/addPatient', addPatient)
+
+//DELETE a single patient
+router.get('/deletePatient/:id', deletPatient)
+
+//UPDATE a single patient
+router.get('/updatePatient/:id', updatePatient)
+
 module.exports = router
