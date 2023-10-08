4fc88a076fef0ac899fd0184cab86b6b40f86d74
Saraghanem <saraghanem2002@gmail.com>
2023-10-08 18:12:26 +0300

    doctor model

--- backend/controllers/doctorController.js
+++
@@ -1,3 +0,0 @@
-const asyncHandler = require('express-async-handler')
-
-const Doctor = require('../models/doctorModel')
--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -1,7 +1,64 @@
 const mongoose = require('mongoose')
-
+const patientdb = require('./patientSchema')
 const doctorSchema = mongoose.Schema({
-    
+
+    First_Name: {
+        type: String,
+        required: true,
+      },
+      Last_Name: {
+        type: String,
+        required: true,
+      },
+      Email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      Age: {
+        type: Number,
+        required: true,
+      },
+      Username: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      Password: {
+        type: String,
+        required: true,
+      },
+      Affiliation: {
+        type: String,
+        required: true,
+      },
+      Hourly_rate: {
+        type: Number,
+      },
+    Wallet_amount: {
+        type : Number,
+        
+    },
+      Avialable_time_slots: {
+        type: timestamps,
+        
+      },
+    Appoitments: [{
+        date: {
+            type:Date ,
+            required: true,
+        },
+        status:{
+            type:String,
+            required: true,
+
+        },
+        pateint_id :{
+            type :Number,
+            required:true
+        }
+
+    }]
 },
 {
     timestamps: true
