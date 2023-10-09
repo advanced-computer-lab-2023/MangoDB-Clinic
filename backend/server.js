c8225c316df7a879b8eeb3abfcb72c22d555a2d4
Kareem Gabr <Kareemjabr26@gmail.com>
2023-10-09 16:48:40 +0300

    modification in server js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,31 @@
 const express = require('express')
-const colors = require('colors')
+// const colors = require('colors')
 require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const guestRoutes = require('./routes/guestRoutes')
 const patientRoutes = require('./routes/patientRoutes')
-const connectDB = require('./config/db')
-const port = process.env.PORT
+// const connectDB = require('./config/db')
+// const port = process.env.PORT
 
 // connectDB()
 
+//express App
 const app = express()
 
+//middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use((req, res, next) => {
+    console.log(req.path, res.method);
+    next()
+})
+
+//routes
 app.use('/', guestRoutes)
 app.use('/patient/', patientRoutes)
 
+//connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         //listen for requests
@@ -25,9 +34,9 @@ mongoose.connect(process.env.MONGO_URI)
         })
     })
     .catch((error) => {
-        console.log(error);
+        console.log( "error in connection", error);
     })
 
-app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server Started On Port ${port}...`.green.bold))
+
+// app.listen(port, () => console.log(`Server Started On Port ${port}...`.green.bold))
