40f6fceed41e5f6acaf1b644f97dbc0c51e9edac
farisebiede <127598352+farisebiede@users.noreply.github.com>
2023-11-07 19:42:27 +0200

    Requirements & patientModel update

--- backend/models/packagesModel.js
+++ backend/models/packagesModel.js
@@ -1,30 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const packagesSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    doctorSessionDiscount: {
-        type: Number,
-        required: true
-    },
-    medicineDiscount: {
-        type: Number,
-        required:true
-    },
-    familyDiscount: {
-        type: Number,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  doctorSessionDiscount: {
+    type: Number,
+    required: true,
+  },
+  medicineDiscount: {
+    type: Number,
+    required: true,
+  },
+  familyDiscount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+  },
+  renewalDate: {
+    type: Date,
+  },
+  cancellationDate: {
+    type: Date,
+  },
+});
 
-module.exports = mongoose.model('Packages', packagesSchema)
+module.exports = mongoose.model("Packages", packagesSchema);
