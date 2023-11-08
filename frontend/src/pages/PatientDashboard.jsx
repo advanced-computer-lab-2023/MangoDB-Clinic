5315110dfc424bef5c4951bdceacdc5a246fed38
Kareem Gabr <Kareemjabr26@gmail.com>
2023-11-08 12:05:23 +0200

    changes in view prescription

--- frontend/src/pages/PatientDashboard.jsx
+++ frontend/src/pages/PatientDashboard.jsx
@@ -17,7 +17,7 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import DoctorsTable from '../components/DoctorsTable';
+// import DoctorsTable from '../components/DoctorsTable';
 import { mainListItems } from '../components/ListItems';
 // import Chart from './Chart';
 // import Deposits from './Deposits';
--- frontend/src/pages/ViewPrescriptions.js
+++ frontend/src/pages/ViewPrescriptions.js
@@ -1,6 +1,52 @@
+import { Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+// import useFetch from "./useFetch";
+
 const ViewPrescriptions = () => {
+    const patient_id = '6526d30a0f83f5e462288354';
+    const [prescriptions, setPrescriptions] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/patient/get_prescriptions_of_patient/' + patient_id)
+        .then(res => {
+            if (!res.ok) {
+                throw Error('Could not fetch the data for that resource');
+            }
+            return res.json();
+        })
+        .then(data => {
+            setPrescriptions(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+        })
+    }, []);
+
     return ( 
-        <h1>View Prescriptions</h1>
+        <Grid container  justifyContent="center" style={{ padding: '2rem' }}>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <h2>Prescriptions</h2>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                { isPending && <div>Loading...</div> }
+                { error && <div>{ error }</div> }
+                { prescriptions && (
+                    <div>
+                        {prescriptions.map((prescription) => (
+                            <div key={prescription._id}>
+                                <h3>{prescription.doctor_name}</h3>
+                                <p>{prescription.prescription}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </Grid>
+        </Grid>
      );
 }
  
