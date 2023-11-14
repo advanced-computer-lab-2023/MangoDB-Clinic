720159e41e54e9913940e3cb176f7cfda2d53ae5
noureldinabdelalim <127529001+noureldinabdelalim@users.noreply.github.com>
2023-11-14 17:06:04 +0200

    Some protect patient and doc added

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -67,76 +67,76 @@ router.get("/get_patient/:id", getPatient);
 router.post("/add_patient", addPatient);
 
 //DELETE a single patient
-router.delete("/delet_patient/:id", deletePatient);
+router.delete("/delet_patient/:id",protectPatient, deletePatient);
 
 //UPDATE a single patient
-router.put("/update_patient/:id", updatePatient);
+router.put("/update_patient/:id",protectPatient, updatePatient);
 
-router.put("/add_family_member/:id", addFamilyMember);
+router.put("/add_family_member/:id",protectPatient, addFamilyMember);
 
-router.get("/get_family_members/:id", getFamilyMembers);
+router.get("/get_family_members/:id",protectPatient, getFamilyMembers);
 
-router.get("/get_selected_doctor/:id", getSelectedDoctor);
+router.get("/get_selected_doctor/:id",protectPatient, getSelectedDoctor);
 
-router.get("/get_all_prescriptions", getAllPrescriptions);
+router.get("/get_all_prescriptions",protectPatient, getAllPrescriptions);
 
 //GET all prescriptions of a single patient
 router.get("/get_prescriptions_of_patient/", protectPatient, getAllPrescriptionsOfPatient);
 
 //filter prescriptions
-router.get("/filter_prescription/:patientId", filterPrescription);
+router.get("/filter_prescription/:patientId",protectPatient, filterPrescription);
 
 //select a prescription from my list of prescriptions
-router.get("/select_prescription/:prescriptionId",  selectPrescription);
+router.get("/select_prescription/:prescriptionId",protectPatient,  selectPrescription);
 
-router.get("/get_all_doctors/:id", viewAllDoctors);
+router.get("/get_all_doctors/:id",protectPatient, viewAllDoctors);
 
-router.get("/filter_doctors/:id", filterDoctors);
+router.get("/filter_doctors/:id",protectPatient, filterDoctors);
 
-router.get("/get_all_appointments/:id", getAllAppointments);
+router.get("/get_all_appointments/:id",protectPatient, getAllAppointments);
 
-router.get("/filter_appointments/:id", filterAppointments);
+router.get("/filter_appointments/:id",protectPatient, filterAppointments);
 
-router.get("/search_doctor/:id", searchDoctor);
+router.get("/search_doctor/:id",protectPatient, searchDoctor);
 
-router.get("/view_health_records/:id", viewHealthRecords);
+router.get("/view_health_records/:id",protectPatient, viewHealthRecords);
 
 router.get("/view_health_packages", viewHealthPackages);
 
-router.get("/view_subscribed_health_package/:id", viewSubscribedhealthPackage);
+router.get("/view_subscribed_health_package/:id",protectPatient, viewSubscribedhealthPackage);
 
-router.put("/cancel_health_package/:id", cancelHealthPackage);
+router.put("/cancel_health_package/:id",protectPatient, cancelHealthPackage);
 
-router.get("/view_wallet/:id", viewWallet);
+router.get("/view_wallet/:id",protectPatient, viewWallet);
 
-router.put("/add_documents/:id", upload.array("document"), addDocuments);
+router.put("/add_documents/:id",protectPatient, upload.array("document"), addDocuments);
 
-router.delete("/delete_document/:patientId/:documentId", deleteDocument);
+router.delete("/delete_document/:patientId/:documentId",protectPatient, deleteDocument);
 
-router.put("/link_family_member/:id", linkFamilyMember);
+router.put("/link_family_member/:id",protectPatient, linkFamilyMember);
 
 router.put(
-  "/subscribe_to_health_package/:patientId/:packageId",
+  "/subscribe_to_health_package/:patientId/:packageId",protectPatient,
   subscribeToHealthPackage
 );
 
 // utils
-router.post("/add_prescription/:doctorId/:patientId", addPrescription);
-router.post("/add_appointment/:doctorId/:patientId", addAppointment);
+router.post("/add_prescription/:doctorId/:patientId",protectPatient, addPrescription);
+router.post("/add_appointment/:doctorId/:patientId",protectPatient, addAppointment);
 router.get("/get_specialities", getSpecialities);
 router.get("/getAvailableAppointments",getAvailableAppointments)
 
 
-router.get("/get_available_appointments/:id", getAvailableAppointments);
+router.get("/get_available_appointments/:id",protectPatient, getAvailableAppointments);
 
-router.post("/make_appointment", makeAppointment)
+router.post("/make_appointment",protectPatient, makeAppointment)
 
 
 router.post('/upcoming', upcoming)
 
-router.post('/filterapp', filterStatus);
+router.post('/filterapp',protectPatient, filterStatus);
 
-router.post('/payFromWallet/:appointmentId', payFromWallet);
+router.post('/payFromWallet/:appointmentId',protectPatient, payFromWallet);
 
 
 module.exports = router;
