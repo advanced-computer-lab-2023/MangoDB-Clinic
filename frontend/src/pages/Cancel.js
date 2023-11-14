3f2b71f8aa629803932f792c23b5ced28f3ceaf9
Dina Mamdouh <102436449+DinaaSaur@users.noreply.github.com>
2023-11-14 19:33:28 +0200

    success and cancel

--- frontend/src/pages/Cancel.js
+++ frontend/src/pages/Cancel.js
@@ -1,7 +1,26 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 const Cancel = () => {
+    const history = useHistory();
+
+    useEffect(() => {
+        // Use setTimeout to delay the redirection
+        const timeoutId = setTimeout(() => {
+            // Redirect to the home page after 5 seconds
+            history.push('/');
+        }, 5000);
+
+        // Clear the timeout when the component is unmounted
+        return () => clearTimeout(timeoutId);
+    }, [history]);
+
     return (
-        <h1>cancel </h1>
-     );
-}
- 
+        <div>
+            <h1>Payment Cancelled</h1>
+            <p>Redirecting to the home page...</p>
+        </div>
+    );
+};
+
 export default Cancel;
