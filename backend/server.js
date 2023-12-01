2ff8f0e5c282a099c00003feacd79e72c0b30a51
Kareem Gabr <Kareemjabr26@gmail.com>
2023-12-01 19:54:27 +0200

    minor modification

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server)
 const colors = require("colors");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -12,41 +15,9 @@ const cors = require("cors");
 
 connectDB();
 
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.get("/doctorHomePage", (req, res) => {
-	res.render("doctorHomePage");
-});
-
-app.get("/viewAllPatients", (req, res) => {
-	res.render("viewAllPatients");
-});
-
-app.get("/searchByName", (req, res) => {
-	res.render("searchByName", { patients: [] });
-});
-app.get("/selectedPatient/:id", async (req, res) => {
-	try {
-		const patientId = req.params.id;
-		const patient = await selectPatient(patientId);
-		if (!patient) {
-			console.error("Patient not found for ID:", patientId);
-		}
-		res.render("selectedPatient", { patient });
-	} catch (error) {
-		console.error("Error retrieving patient:", error);
-
-		res.status(500).json({ error: "Error retrieving patient" });
-	}
-});
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
 app.use((req, res, next) => {
 	console.log(req.path, req.method);
