732efff2abdff5fddee26c51106f8d6b19272f87
Kareem Gabr <Kareemjabr26@gmail.com>
2023-12-03 23:17:17 +0200

    modifications in doctors table

--- frontend/src/components/DoctorsTable.js
+++ frontend/src/components/DoctorsTable.js
@@ -1,8 +1,49 @@
-import useFetch from "../useFetch";
+import { useState, useEffect } from "react";
 import ReusableTable2 from "./ReusableTable2";
 
 const DoctorsTable = ({ url }) => {
-	const { data: doctors, error, isPending } = useFetch(url);
+	// const { data: doctors, error, isPending } = useFetch(url);
+
+	const [doctors, setDoctors] = useState([]);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+		  try {
+			  setDoctors([]);
+			  setIsPending(true);
+			  setError(null);
+
+			const token = localStorage.getItem("token");
+
+			const response = await fetch(url,
+			{
+				method: "GET",
+				headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+				},
+			}
+			);
+
+			if (!response.ok) {
+				throw new Error("Could not fetch the data for that resource");
+			}
+
+			const data = await response.json();
+			setDoctors(data);
+			setIsPending(false);
+			setError(null);
+			  
+		  } catch (err) {
+		  setIsPending(false);
+		  setError(err.message);
+		  }
+	  };
+  
+	  fetchData();
+	  }, [url]);
 
 	const columns = [
 		{ id: "doctorName", label: "Doctor Name" },
