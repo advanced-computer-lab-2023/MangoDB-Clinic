032edba6957511f02fc97bb9bfb5cb80707d3f08
OmarElZaher <omarelzaher93@gmail.com>
2023-12-18 04:45:34 +0200

    style

--- frontend/src/components/EmploymentContract.jsx
+++ frontend/src/components/EmploymentContract.jsx
@@ -65,7 +65,7 @@ export default function EmploymentContract() {
 	const handleReturn = async () => {
 		localStorage.removeItem("token");
 		navigate("/login");
-	}
+	};
 	const handleAccept = async () => {
 		try {
 			setIsLoading(true);
@@ -153,81 +153,96 @@ export default function EmploymentContract() {
 				<Spinner />
 			) : (
 				<>
-				
-					<Typography variant='h2' gutterBottom>
+					<Typography variant='h1' gutterBottom sx={{pl: "415px", pt: "10px"}}>
 						EMPLOYMENT AGREEMENT
 					</Typography>
-					<div style={{ display: 'flex', justifyContent: 'center' }}>
-					<Paper sx={{ width: '70%', margin: 'auto', marginBottom: 5 }}>
-	<Paper>
-					<Typography variant="p" > 
-						This Employment Agreement ("Agreement") is entered into on{" "}
-						{new Date().toDateString()} by and between EL7A2NY VIRTUAL CLINIC
-						("Employer"), and {doctor.firstName} {doctor.lastName}, a licensed
-						physician ("Doctor").
-					</Typography>
-					<Typography>1. POSITION AND DUTIES</Typography>
-					<Typography>
-						Employer agrees to employ Doctor as a {doctor.speciality} at EL7A2NY
-						VIRTUAL CLINIC. Doctor agrees to diligently and professionally
-						perform the duties and responsibilities associated with the
-						position, as determined by Employer.
-					</Typography>
-					<Typography>2. COMPENSATION</Typography>
-					<Typography>
-						Employer agrees to pay Doctor a salary of {doctor.hourlyRate} per
-						hour. Payment will be made on an hourly basis via In App Wallet.
-					</Typography>
-					<Typography>3. WORK HOURS</Typography>
-					<Typography>
-						Doctor agrees to work desired hours per week, with specific workdays
-						and hours to be mutually agreed upon by Doctor and Employer.
-					</Typography>
-					<Typography>4. TERM OF EMPLOYMENT</Typography>
-					<Typography>
-						This Agreement shall commence on {new Date().toDateString()} and
-						continue until terminated by either party.
-					</Typography>
-					<Typography>5. LICENSES AND CREDENTIALS</Typography>
-					<Typography>
-						Doctor represents and warrants that they possess all necessary
-						licenses and credentials required to practice medicine in Egypt.
-					</Typography>
-					<Typography>6. CONFIDENTIALITY</Typography>
-					<Typography>
-						Doctor agrees to maintain the confidentiality of patient information
-						and all proprietary information of the clinic/hospital.
-					</Typography>
-					<Typography>7. TERMINATION</Typography>
-					<Typography>
-						Either party may terminate this Agreement with or without cause upon
-						written notice to the other party.
-					</Typography>
-					<Typography>8. GOVERNING LAW</Typography>
-					<Typography>
-						This Agreement shall be governed by and construed in accordance with
-						the laws of the Arab Republic of Egypt.
-					</Typography>
-					<br />
-					<Typography>
-						IN WITNESS WHEREOF, the parties hereto have executed this Agreement
-						as of the date first above written.
-						<br></br>
-					</Typography>
-					</Paper>
-					</Paper>
+					<div style={{ display: "flex", justifyContent: "center" }}>
+						<Paper sx={{ width: "70%", margin: "auto", marginBottom: 5 }}>
+							<Paper sx={{padding:"10px"}}>
+								<Typography variant='p'>
+									This Employment Agreement ("Agreement") is entered into on{" "}
+									{new Date().toDateString()} by and between EL7A2NY VIRTUAL
+									CLINIC ("Employer"), and {doctor.firstName} {doctor.lastName},
+									a licensed physician ("Doctor").
+								</Typography>
+								<Typography>1. POSITION AND DUTIES</Typography>
+								<Typography>
+									Employer agrees to employ Doctor as a {doctor.speciality} at
+									EL7A2NY VIRTUAL CLINIC. Doctor agrees to diligently and
+									professionally perform the duties and responsibilities
+									associated with the position, as determined by Employer.
+								</Typography>
+								<Typography>2. COMPENSATION</Typography>
+								<Typography>
+									Employer agrees to pay Doctor a salary of {doctor.hourlyRate}{" "}
+									per hour. Payment will be made on an hourly basis via In App
+									Wallet.
+								</Typography>
+								<Typography>3. WORK HOURS</Typography>
+								<Typography>
+									Doctor agrees to work desired hours per week, with specific
+									workdays and hours to be mutually agreed upon by Doctor and
+									Employer.
+								</Typography>
+								<Typography>4. TERM OF EMPLOYMENT</Typography>
+								<Typography>
+									This Agreement shall commence on {new Date().toDateString()}{" "}
+									and continue until terminated by either party.
+								</Typography>
+								<Typography>5. LICENSES AND CREDENTIALS</Typography>
+								<Typography>
+									Doctor represents and warrants that they possess all necessary
+									licenses and credentials required to practice medicine in
+									Egypt.
+								</Typography>
+								<Typography>6. CONFIDENTIALITY</Typography>
+								<Typography>
+									Doctor agrees to maintain the confidentiality of patient
+									information and all proprietary information of the
+									clinic/hospital.
+								</Typography>
+								<Typography>7. TERMINATION</Typography>
+								<Typography>
+									Either party may terminate this Agreement with or without
+									cause upon written notice to the other party.
+								</Typography>
+								<Typography>8. GOVERNING LAW</Typography>
+								<Typography>
+									This Agreement shall be governed by and construed in
+									accordance with the laws of the Arab Republic of Egypt.
+								</Typography>
+								<br />
+								<Typography>
+									IN WITNESS WHEREOF, the parties hereto have executed this
+									Agreement as of the date first above written.
+									<br></br>
+								</Typography>
+							</Paper>
+						</Paper>
 					</div>
 
-<div style={{ display: 'flex', justifyContent: 'center' }} >
-					<Button variant='contained' onClick={handleAccept} sx={{ width: '20%', margin: 'auto' }}>
-						Accept
-					</Button>
-					<Button variant='contained' onClick={handleReturn} sx={{ width: '20%', margin: 'auto' }}>
-						Return To Login
-					</Button>
-					<Button variant='outlined' onClick={handleReject} sx={{ width: '20%', margin: 'auto' }}>
-						Reject
-					</Button>
+					<div style={{ display: "flex", justifyContent: "center" }}>
+						<Button
+							variant='contained'
+							onClick={handleAccept}
+							sx={{ width: "20%", margin: "auto" }}
+						>
+							Accept
+						</Button>
+						<Button
+							variant='contained'
+							onClick={handleReturn}
+							sx={{ width: "20%", margin: "auto" }}
+						>
+							Return To Login
+						</Button>
+						<Button
+							variant='outlined'
+							onClick={handleReject}
+							sx={{ width: "20%", margin: "auto" }}
+						>
+							Reject
+						</Button>
 					</div>
 					{isSuccess ? (
 						<Snackbar
